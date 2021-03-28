f1aac6ed8ade6d18ef8bd0336acac6d2180e1c75
Siddharth VP <siddharthvp@gmail.com>
2021-03-29 00:50:57 +0530

    add some custom i18n plugins, improve message fetching

--- src/messenger.ts
+++ src/messenger.ts
@@ -1,15 +1,36 @@
-import Banana, { Messages } from "orange-i18n";
-import messages from "../i18n/en.json";
-import MWMessages from "./mw-messages";
+import Banana, { Messages } from 'orange-i18n';
+import messages from '../i18n/en.json';
+import MWMessages from './mw-messages';
 
-let banana = new Banana(mw.config.get("wgContentLanguage"));
+let banana = new Banana(mw.config.get('wgContentLanguage'));
+
+banana.registerParserPlugin('date', (nodes) => {
+	// Check if it's already Date or Morebits.date object
+	// otherwise assume we can construct a Morebits.date
+	// out of it
+	let mbDate =
+		typeof nodes[0].getTime === 'function' ? new Morebits.date(nodes[0].getTime()) : new Morebits.date(nodes[0]);
+
+	let format = nodes[1];
+	// XXX: Don't assume the wiki's timezone is UTC
+	let zone: 'utc' | undefined = nodes[2] === 'utc' ? 'utc' : undefined;
+	if (format === 'relative') {
+		return mbDate.calendar(zone);
+	} else {
+		return mbDate.format(format, zone);
+	}
+});
+
+banana.registerParserPlugin('sysop', (nodes) => {
+	return Morebits.userIsSysop ? nodes[1] : nodes[2];
+});
 
 /**
  * Load messages into the message store.
  * @param messages
  */
 export function loadMessages(messages: Messages) {
-	banana.load(messages, mw.config.get("wgContentLanguage"));
+	banana.load(messages, mw.config.get('wgContentLanguage'));
 }
 
 /**
@@ -39,16 +60,47 @@ export function initMessaging() {
  * Load the messages available in MediaWiki using the API.
  * These will include generic items such as month/day names, etc.
  * See mw-messages.ts for the list of keys.
+ * TODO: messages in per-wiki config should override these MW messages,
+ * not the other way round
  */
 function loadMediaWikiMessages() {
-	return new mw.Api()
-		.getMessages(MWMessages, {
-			amlang: mw.config.get("wgContentLanguage"),
-			// cache them, as messages are not going to change that often
-			maxage: 99999999,
-			smaxage: 99999999,
-		})
-		.then((mwMessages) => {
-			loadMessages(mwMessages);
-		});
+	let promises = [];
+	for (let i = 0; i < MWMessages.length; i += 50) {
+		promises.push(
+			// Mediawiki namespace-based overrides of MW messages do get taken
+			// into account
+			new mw.Api()
+				.getMessages(MWMessages.slice(i, i + 50), {
+					amlang: mw.config.get('wgContentLanguage'),
+					// cache them, as messages are not going to change that often
+					maxage: 31536000, // 1 year
+					smaxage: 31536000,
+					// uselang enables public caching, see https://phabricator.wikimedia.org/T97096
+					uselang: 'content',
+				})
+				.then((mwMessages) => {
+					loadMessages(mwMessages);
+				})
+		);
+	}
+	return $.when.apply(null, promises);
+}
+
+/**
+ * Load messages from a web address.
+ * @param url
+ */
+export function loadMessagesFromWeb(url) {
+	return $.getJSON(url).then((data) => loadMessages(data));
+	// XXX: this code better handles caching, but is giving CORS issue
+	// $.ajax({
+	// 	url: url,
+	// 	dataType: 'json',
+	// 	cache: true,
+	// 	headers: {
+	// 		'Cache-Control': 'max-age=864000', // 10 days
+	// 	},
+	// }).then((data) => {
+	// 	loadMessages(JSON.parse(data));
+	// });
 }
