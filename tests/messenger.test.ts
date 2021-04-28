3f54b452d2aa5bef8c2dfbc9257139f3aade74c3
Siddharth VP <siddharthvp@gmail.com>
2021-04-28 21:57:56 +0530

    Add unit tests for messenger
    Also fixed broken tests/tsconfig.json

---
+++ tests/messenger.test.ts
@@ -0,0 +1,43 @@
+import './test_base';
+
+import { msg, addMessages, initMessaging } from '../src/messenger';
+
+describe('messenger', () => {
+	beforeAll(() => {
+		return initMessaging().then(() => {
+			addMessages({
+				'test-msg-1': 'Test message 1',
+				'and': ' and',
+				'word-separator': ' ',
+				'comma-separator': ', ',
+			});
+		});
+	});
+
+	test('addMessages and msg', () => {
+		expect(msg('test-msg-1')).toEqual('Test message 1');
+		addMessages({
+			'test-msg-2': 'Test message 2',
+		});
+		expect(msg('test-msg-1')).toEqual('Test message 1');
+		expect(msg('test-msg-2')).toEqual('Test message 2');
+	});
+
+	test('i18n parser plugins', () => {
+		expect(msg('{{date:$1|D MMM YYYY}}', '4 December 2021')).toEqual('4 Dec 2021');
+		expect(msg('{{date:$1|D MMM YYYY}}', new Date('4 December 2021'))).toEqual('4 Dec 2021');
+
+		expect(msg('{{int:test-msg-1}}')).toEqual('Test message 1');
+
+		expect(msg('{{ns:3}}')).toEqual('User talk');
+		expect(msg('{{ns:special}}')).toEqual('Special');
+
+		expect(msg('{{list:$1}}', ['1', '2', '3'])).toEqual('1, 2 and 3');
+
+		expect(msg('{{ucfirst:hello}}')).toEqual('Hello');
+		expect(msg('{{lcfirst:Hello}}')).toEqual('hello');
+		expect(msg('{{lcfirst:hello}}')).toEqual('hello');
+	});
+
+	// TODO: spy on mw.Api and add test for loadAdditionalMediaWikiMessages
+});
--- tests/test_base.ts
+++ tests/test_base.ts
@@ -2,3 +2,14 @@ import 'mock-mediawiki';
 
 require(__dirname + '/../morebits/morebits');
 global.Morebits = window.Morebits;
+
+// @ts-ignore
+mw.libs.pluralRuleParser = require('cldrpluralruleparser');
+
+// Stubs:
+mw.Api = class {
+	// For messenger.test.ts
+	getMessages() {
+		return Promise.resolve({});
+	}
+};
